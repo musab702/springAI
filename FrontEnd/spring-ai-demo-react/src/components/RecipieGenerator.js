9fac3c5e9b4ad561c38f825b2afa350ea41d668a
musab702 <musabshaikh702@gmail.com>
2024-12-25 21:59:48 -0500

    frontend finalised

--- FrontEnd/spring-ai-demo-react/src/components/RecipieGenerator.js
+++ FrontEnd/spring-ai-demo-react/src/components/RecipieGenerator.js
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+
 
 function RecipieGenerator() {
+
+    const [ingredients, setIngredients] = useState('');
+    const [cuisine, setCuisine] = useState('any');
+    const [dietaryRestrictions, setDietaryRestrictions] = useState('');
+    const [recipie, setRecipie] = useState('');
+
+
+        const createRecipie = async()=>{
+            try {
+           
+                const response = await fetch(`http://localhost:8080/recipie-creator?ingredients=${ingredients}dietaryRestrictions=${dietaryRestrictions}&cuisine=${cuisine}`)
+                const data = await response.text();
+                console.log(data);
+                setRecipie(data);
+                
+            } catch (error) {
+                console.error("Error generating recipie: ", error)
+                
+            }
+        };
+
+
     return (
-        <h2>Generate recipie</h2>
+        <div>
+            <h2>Generate recipie</h2>
+            <input 
+                type="text"
+                value={ingredients}
+                onChange={(e)=>setIngredients(e.target.value)}
+                placeholder="Enter ingredients (comma separated)"
+            />
+
+            <input 
+                type="text"
+                value={cuisine}
+                onChange={(e)=>setCuisine(e.target.value)}
+                placeholder="Enter Cuisine"
+            />
+
+            <input 
+                type="text"
+                value={dietaryRestrictions}
+                onChange={(e)=>setDietaryRestrictions(e.target.value)}
+                placeholder="Enter dietary restriction"
+            />
+        <button onClick={createRecipie}>Create Recipie</button>
+
+        <div className="output">
+            <pre className="recipe-text">{recipie}</pre>
+        </div>
+
+        </div>  
     );
 }
 
--- FrontEnd/spring-ai-demo-react/src/components/chat.js
+++ FrontEnd/spring-ai-demo-react/src/components/chat.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Chat() {
+    const [prompt, setPrompt] = useState('');
+    const [chatResponse, setChatResponse] = useState('');
+
+    const askAI = async()=>{
+        try {
+       
+            const response = await fetch(`http://localhost:8080/ask-ai-options?prompt=${prompt}`)
+            const data = await response.text();
+            console.log(data);
+            setChatResponse(data);
+            
+        } catch (error) {
+            console.error("Error generating chat response: ", error)
+            
+        }
+    };
+
     return (
-        <h2>Chat</h2>
+        <div>
+            <h2>Talk to AI</h2>
+            <input
+                type="text"
+                value={prompt}
+                onChange={(e)=> setPrompt(e.target.value)}
+                placeholder="Enter a Prompt for AI"
+            />
+            <button onClick={askAI}>Ask AI</button>
+            <div className="output">
+                <p>{chatResponse}</p>
+            </div>              
+            
+        </div>
     );
 }
 
